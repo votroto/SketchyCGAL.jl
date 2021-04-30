e22dd2dfe82e5bee1b2a7e53030c5494c4a4ac39
tomas votroubek <tomas.votroubek@outlook.com>
2021-04-30 17:40:16 +0200

    add progress meter

--- src/eig.jl
+++ src/eig.jl
@@ -8,10 +8,6 @@ Return the approximate smallest eigenvalue and eigenvector of a matrix
 `M` in `iters` iterations.
 """
 function approx_eigmin(M, iters)
-	n = size(M, 1)
-	
-	initial = normalize(randn(eltype(M), n))
-	values, vecs = eigs(M, nev=1, maxiter=iters, which=:SM, v0=initial)
-
+	values, vecs = eigs(M, nev=1, tol=1e-3, maxiter=iters, check=1)	
 	values[1], vecs[:, 1]
 end
--- src/sketchy_cgal.jl
+++ src/sketchy_cgal.jl
@@ -1,4 +1,5 @@
 using LinearAlgebra
+using ProgressMeter
 
 include("eig.jl")
 include("nystrom.jl")
@@ -6,7 +7,7 @@ include("nystrom.jl")
 """
 	mix(ws, xs)
 
-Linearly combine `xs` weighted by `ws`.
+Linearly combine `xs` weighted by `ws`. A "generalized dot product."
 """
 mix(ws, xs) = sum(ws .* xs)
 
@@ -20,8 +21,8 @@ Solve a trace constrained SDP:
 			X ∈ Symmetric PSD;
 			tr(X) = 1,
 
-where `C, As[1], ..., As[m]` are symmetric matrices of size `n`, `b` is a vector
-of length `m`.
+where `C, As[1], ..., As[m]` are symmetric matrices of size `n` with norm 1; 
+`b` is a vector of length `m`.
 """
 function sketchy_cgal(C, As, b; R, iterations=1e3, β=1)
 	n = size(C, 1)
@@ -31,7 +32,7 @@ function sketchy_cgal(C, As, b; R, iterations=1e3, β=1)
 	z = zeros(m)
 	y = zeros(m)
 
-	for t in 1:iterations
+	@showprogress for t in 1:iterations
 		βt = β * sqrt(t + 1)
 		qt = min(round(Int, t^0.25 * log(n)), n - 1)
 		η = 2 / (t + 1)
